4d6bdfaf7f2f14d2ae09abef71be1f85fc48d85b
Vedangi Patel <vedangipatel@v1040-wn-rt-b-122-50.campus-dynamic.uwaterloo.ca>
2023-01-18 11:49:25 -0500

    second commit

--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -135,7 +135,7 @@ class Home extends Component {
           >
             {this.state.mode === 0 ? (
               <React.Fragment>
-                Welcome to MSci245!
+                Welcome!
               </React.Fragment>
             ) : (
               <React.Fragment>
@@ -147,7 +147,6 @@ class Home extends Component {
         </Grid>
       </Grid>
     )
-
 
     return (
       <MuiThemeProvider theme={theme}>
