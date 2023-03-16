40872f4cd93b2cae3daf8ae80d1fa90c57345b33
yashvi-parmar <yparmar@uwaterloo.ca>
2023-03-16 15:01:54 -0400

    changes

--- client/src/components/TermsAndConditions/index.js
+++ client/src/components/TermsAndConditions/index.js
@@ -34,12 +34,12 @@ const theme = createTheme({
     root: {
       body: {
         backgroundColor: "#000000",
-        opacity: opacityValue,
+        opacity: 0.75,
         overflow: "hidden",
       },
     },
     mainMessage: {
-      opacity: opacityValue,
+      opacity: 0.75,
     },
   
     mainMessageContainer: {
@@ -53,7 +53,7 @@ const theme = createTheme({
       overflow: "hidden",
     },
     message: {
-      opacity: opacityValue,
+      opacity: 0.75,
       maxWidth: 250,
       paddingBottom: theme.spacing(2),
     },
