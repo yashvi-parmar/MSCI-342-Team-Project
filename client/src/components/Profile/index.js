b233cde5e8f6807757ee10af5c5cfe0a13b2f782
VedangiPatel <v238pate@uwaterloo.ca>
2023-03-16 13:21:44 -0400

    Terms and conditions done

---
+++ client/src/components/Profile/index.js
@@ -0,0 +1,90 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import CssBaseline from "@material-ui/core/CssBaseline";
+import { MuiThemeProvider, createTheme } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import Paper from "@material-ui/core/Paper";
+import Box from '@material-ui/core/Box';
+import {Avatar, TextField, Button, Link } from '@material-ui/core'
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import SignIn from '../SignIn'
+import { BrowserRouter,Switch,Route} from 'react-router-dom';
+import Navbar from '../NavBar';
+
+
+const cardStyle={padding :30, height:'60vh',width:380, marginTop: "30px", margin:"20px auto"}
+const buttonStyle={margin:'8px 0', backgroundColor: 'black', color: 'white'}
+const textStyle={marginBottom: '8px'}
+const serverURL = "http://ec2-18-216-101-119.us-east-2.compute.amazonaws.com:3060";
+
+const theme = createTheme({
+    palette: {
+      type: 'light',
+      background: {
+        default: "#042913"
+      },
+      primary: {
+        main: "#b08968",
+      },
+      secondary: {
+        main: "#94b395",
+      },
+    },
+  });
+  
+  const styles = theme => ({
+    root: {
+      body: {
+        backgroundColor: "#000000",
+        opacity: opacityValue,
+        overflow: "hidden",
+      },
+    },
+    mainMessage: {
+      opacity: opacityValue,
+    },
+  
+    mainMessageContainer: {
+      marginTop: "10vh",
+      marginLeft: theme.spacing(10),
+      [theme.breakpoints.down('xs')]: {
+        marginLeft: theme.spacing(4),
+      },
+    },
+    paper: {
+      overflow: "hidden",
+    },
+    message: {
+      opacity: opacityValue,
+      maxWidth: 250,
+      paddingBottom: theme.spacing(2),
+    },
+  
+  });
+
+function Profile() {
+    return (
+        <div> 
+          <Navbar></Navbar>
+
+          <Grid
+            container
+            spacing={0}
+            direction="column"
+            justify="flex-start"
+            alignItems="flex-start"
+            style={{backgroundColor: '#6F4E37', padding: '4vh'}}
+          >
+            <h3>Your profile</h3>
+
+
+          </Grid>
+        </div>     
+      )
+}
+
+export default Profile;
